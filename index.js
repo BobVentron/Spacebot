ac2020b6ed4b8ff02bd41c20c9c7d512088823d9
BobVentron <62252156+BobVentron@users.noreply.github.com>
2021-04-30 22:05:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { platform } = require('os');
 const { ESTALE } = require('constants');
 const fs = require("fs");
 
-bot.login("ODA0NDAwODg0NjUzMDk2OTYw.YBLysw.wWiFNZB8GGaaOYpGVE1LFyAMvyE")
+bot.login("TOKEN")
 let prefix = "/";
 
 const bdd = require("./bdd.json");
